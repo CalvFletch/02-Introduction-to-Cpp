1f877151c0911539fe51fc15e8400034a2853f85
CalvFletch <CIsaacFletcher@gmail.com>
2026-03-10 10:55:35 +0800

    Project resturcture

--- Task 1/Part A/02-FizzBuzz.cpp
+++ Task 1/Part A/02-FizzBuzz.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
+	//  You are to make a function that returns the FizzBuzz string for any given input integer.An
+	//	integer will return “Fizz” if the number is a multiple of 3. It will return “Buzz” if it’s a multiple of
+	//	5. It will return “FizzBuzz” if it’s a multiple of 3 AND 5. Otherwise it will return the input number
+	//	converted to a string.
+	//	Commit this to version control.
+	int user_input;
 
+	std::cout << "give number: ";
+	std::cin >> user_input;
+
+	std::cout << user_input;
 }
