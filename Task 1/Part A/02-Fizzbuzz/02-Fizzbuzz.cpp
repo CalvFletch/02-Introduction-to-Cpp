c417dac0841d703d14cbb9c80dd1f9721839a49f
CalvFletch <CIsaacFletcher@gmail.com>
2026-03-31 14:47:41 +0800

    added step 1 of fizz buzz

--- Task 1/Part A/02-Fizzbuzz/02-Fizzbuzz.cpp
+++ Task 1/Part A/02-Fizzbuzz/02-Fizzbuzz.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
+
+std::string fizzbuzz(int input)
+{
+    if (input % 3 == 0 && input % 5 == 0)
+        return "FizzBuzz";
+    if (input % 3 == 0)
+        return "Fizz";
+    if (input % 5 == 0)
+        return "Buzz";
+    return std::to_string(input);
+}
 
 int main()
 {
-    int userInput;
-    std::cout << "Enter number: ";
-    std::cin >> userInput;
-
-    std::cout << userInput;
+    std::cout << fizzbuzz(15) << "\n";
+    std::cout << fizzbuzz(7) << "\n";
+    std::cout << fizzbuzz(25) << "\n";
+    std::cout << fizzbuzz(99) << "\n";
 }
