a544a2e4ac97f3df515113f9007f9ff43a97219d
CalvFletch <CIsaacFletcher@gmail.com>
2026-04-01 20:22:27 +0800

    added assertions and comments

--- Task 1/Part B/06-Linear-Search/06-Linear-Search.cpp
+++ Task 1/Part B/06-Linear-Search/06-Linear-Search.cpp
@@ -2,6 +2,14 @@
 #include <cassert>
 using namespace std;
 
+/// <summary>
+/// Linearly search through array until the target input is found then
+/// returns it's index.
+/// </summary>
+/// <param name="arr"> Input array to be searched </param>
+/// <param name="size"> length of the input array </param>
+/// <param name="target"> Int target that is being searched for </param>
+/// <returns> The index of the target being searched; else -1 </returns>
 int linearSearch(int arr[], int size, int target)
 {
     for (int i = 0; i < size; i++)
@@ -15,8 +23,12 @@ int main()
     int myArray[] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
     int arraySize = sizeof(myArray) / sizeof(myArray[0]);
 
+    // Assert test
+    assert(linearSearch(myArray, arraySize, 3) == 2);
+    assert(linearSearch(myArray, arraySize, 7) == 11);
+    assert(linearSearch(myArray, arraySize, 67) == 0);
+    assert(linearSearch(myArray, arraySize, 88) == -1); //value not in the array
+
     cout << linearSearch(myArray, arraySize, 3) << endl;
-    cout << linearSearch(myArray, arraySize, 7) << endl;
-    cout << linearSearch(myArray, arraySize, 67) << endl;
-    cout << linearSearch(myArray, arraySize, 88) << endl; //value not in the array
+
 }
