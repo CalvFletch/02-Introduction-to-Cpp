b54c9be63e41d4b759ab8a1295b63dc87cce04d6
CalvFletch <CIsaacFletcher@gmail.com>
2026-04-01 13:13:44 +0800

    added assert created assert tests based on table

--- Task 1/Part A/04-Statistics/04-Statistics.cpp
+++ Task 1/Part A/04-Statistics/04-Statistics.cpp
@@ -14,8 +14,16 @@ int calculateAccuracy(int hits, int fired)
 // Run Calc Accuracy with 3 hits for 4 shots
 int main()
 {
-    // Test calculate validity
+    // Test calculate validity with given table data 
+    // and then print the same data
     assert(calculateAccuracy(3, 4) == 75);
-   
+    cout << calculateAccuracy(3, 4) << "%" << endl;
+    assert(calculateAccuracy(1, 2) == 50);
+    cout << calculateAccuracy(1, 2) << "%" << endl;
+    assert(calculateAccuracy(13, 13) == 100);
+    cout << calculateAccuracy(13, 13) << "%" << endl;
+    assert(calculateAccuracy(21, 173) == 12);
+    cout << calculateAccuracy(21, 173) << "%" << endl;
+    assert(calculateAccuracy(0, 0) == 0);
     cout << calculateAccuracy(0, 0) << "%" << endl;
 }
