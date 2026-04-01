74ab910007452c1ac848b0bc32866d634518d33f
CalvFletch <CIsaacFletcher@gmail.com>
2026-04-01 14:33:41 +0800

    I might have skipped a bit, but this is where we are

--- Task 1/Part A/05-Switch/05-Switch.cpp
+++ Task 1/Part A/05-Switch/05-Switch.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+enum PotionType
+{
+	HEALTH,
+	SPEED,
+	STRENGTH,
+	MAGIC,
+};
+
+struct Player 
+{ 
+	int health = 30; 
+	int speed = 10; 
+	int strength = 15; 
+	int magic = 30; 
+};
+
+// Switch case for applying different types of potions and their values
+Player applyPotion(PotionType type, Player player)
+{
+	switch (type)
+	{
+	case HEALTH:
+		player.health = 100;
+		break;
+	case SPEED:
+		player.speed = 50;
+		player.health -= 10;
+		player.strength -= 10;
+		player.magic = 10;
+		break;
+	case STRENGTH:
+		player.speed = 25;
+		player.health *= 2;
+		player.strength = 200;
+		break;
+	case MAGIC:
+		player.strength = 10;
+		player.magic = 100;
+		break;
+	default:
+		break;
+	}
+	return player;
+}
+
+void printStats(Player input)
+{
+	cout << "Health: " << input.health << endl;
+	cout << "Speed: " << input.speed << endl;
+	cout << "Strength: " << input.strength<< endl;
+	cout << "Magic: " << input.magic << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Player testPlayer;
+	// Start
+	printStats(testPlayer);
+	// Takes potions
+	applyPotion(HEALTH, testPlayer);
+	applyPotion(SPEED, testPlayer);
+	// End
+	printStats(testPlayer);
 }
