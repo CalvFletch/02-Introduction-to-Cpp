ac0f764e39355f729e7ad6de30885f94890939fd
CalvFletch <CIsaacFletcher@gmail.com>
2026-04-01 19:54:11 +0800

    partially completed Step 1

--- Task 1/Part B/06-Linear-Search/06-Linear-Search.cpp
+++ Task 1/Part B/06-Linear-Search/06-Linear-Search.cpp
@@ -1,9 +1,22 @@
-// 06-Linear-Search.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cassert>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+        if (target == arr[i])
+            return i;
+    return -1;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int myArray[] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
+    int arraySize = sizeof(myArray) / sizeof(myArray[0]);
+
+    cout << linearSearch(myArray, arraySize, 3) << endl;
+    cout << linearSearch(myArray, arraySize, 7) << endl;
+    cout << linearSearch(myArray, arraySize, 67) << endl;
+    cout << linearSearch(myArray, arraySize, 88) << endl; //value not in the array
 }
