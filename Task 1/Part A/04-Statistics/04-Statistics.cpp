841223004c79db148073e1c591e943ffa83e6d54
CalvFletch <CIsaacFletcher@gmail.com>
2026-04-01 13:03:44 +0800

    added assert inside calculate accuracy

--- Task 1/Part A/04-Statistics/04-Statistics.cpp
+++ Task 1/Part A/04-Statistics/04-Statistics.cpp
@@ -2,12 +2,20 @@
 #include <cassert>
 using namespace std;
 
+// Compares hits to fired shots and returns percentage
 int calculateAccuracy(int hits, int fired)
 {
+    assert(hits <= fired && fired >= 0);
+    if (fired == 0) // handle 0 fired gracefully
+        return 0;
     return 100 * hits / fired;
 }
+
+// Run Calc Accuracy with 3 hits for 4 shots
 int main()
 {
+    // Test calculate validity
     assert(calculateAccuracy(3, 4) == 75);
-    cout << calculateAccuracy(3, 4) << "%" << endl;
+   
+    cout << calculateAccuracy(0, 0) << "%" << endl;
 }
