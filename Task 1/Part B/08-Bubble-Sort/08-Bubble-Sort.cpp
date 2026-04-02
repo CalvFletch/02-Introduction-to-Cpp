c49216c9f49e4ad5d6d92a4de636830f1a28d194
CalvFletch <CIsaacFletcher@gmail.com>
2026-04-02 09:48:03 +0800

    Added main logic for bubble sort

--- Task 1/Part B/08-Bubble-Sort/08-Bubble-Sort.cpp
+++ Task 1/Part B/08-Bubble-Sort/08-Bubble-Sort.cpp
@@ -1,9 +1,29 @@
-// 08-Bubble-Sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cassert>
+using namespace std;
+
+
+void bubbleSort(int arr[], int size)
+{
+    bool swapped = true;
+    while (swapped = true) {
+        for (int i = 0; i < size - 1; i++) {
+            bool swapped = false;
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i] = temp;
+                swapped = true;
+            };
+        };
+    };
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int myArray[] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
+    int arraySize = sizeof(myArray) / sizeof(myArray[0]);
+    bubbleSort(myArray, arraySize);
+
 }
